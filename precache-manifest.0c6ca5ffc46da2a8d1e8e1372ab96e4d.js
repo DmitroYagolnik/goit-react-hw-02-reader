5966c1e8dd72d5091c1af983b8549bc40723753b
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2019-12-05 16:59:51 +0200

    Updates

---
+++ precache-manifest.0c6ca5ffc46da2a8d1e8e1372ab96e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9c3e04dae872cfea9b7e3f325f8c78",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "7021035ca9e2bc0baac9",
+    "url": "/goit-react-hw-02-reader/static/css/main.a1569ed8.chunk.css"
+  },
+  {
+    "revision": "f421b5d7a19dc8f73f0c",
+    "url": "/goit-react-hw-02-reader/static/js/2.0aba6b90.chunk.js"
+  },
+  {
+    "revision": "7021035ca9e2bc0baac9",
+    "url": "/goit-react-hw-02-reader/static/js/main.4a38be60.chunk.js"
+  },
+  {
+    "revision": "58b57449e5428d7bb638",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.4603fe05.js"
+  }
+]);
